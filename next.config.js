8186d24728b8c359026737d1fdf225905f23ff81
Shahir <mohamedshahirsirajudeen@gmail.com>
2023-09-14 23:44:33 +0800

    adjusted eslint

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        GITHUB_TOKEN:"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"
+    }
+}
+
+module.exports = nextConfig
